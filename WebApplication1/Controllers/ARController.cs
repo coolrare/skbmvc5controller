fd44c9dd59b7fd97ad142a3c166a288afb920022
Will <doggy.huang@gmail.com>
2020-09-30 10:27:53 +0800

    建立 ARController 並練習 ViewResult 與 PartialViewResult 使用方式

---
+++ WebApplication1/Controllers/ARController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ARController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewResultTest1()
+        {
+            return View();
+        }
+        public ActionResult ViewResultTest2()
+        {
+            return View("ViewResultTest1");
+        }
+        public ActionResult ViewResultTest3()
+        {
+            var data = new ViewResultTestModel();
+
+            return View("ViewResultTest1", data);
+        }
+        public ActionResult ViewResultTest4()
+        {
+            return View();
+        }
+
+        public ActionResult PartialViewResultTest1()
+        {
+            return PartialView();
+        }
+        public ActionResult PartialViewResultTest2()
+        {
+            return PartialView("ViewResultTest1");
+        }
+        public ActionResult PartialViewResultTest3()
+        {
+            var data = new ViewResultTestModel();
+
+            return PartialView("ViewResultTest1", data);
+        }
+        public ActionResult PartialViewResultTest4()
+        {
+            return PartialView();
+        }
+
+    }
+
+    public class ViewResultTestModel
+    {
+        public int Id { get; set; } = 1;
+
+        public string Name { get; set; } = "Will";
+    }
+}
