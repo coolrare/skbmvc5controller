27878d79b807eb6a98446cd4cef7b11758b8365e
Will <doggy.huang@gmail.com>
2020-09-30 11:03:48 +0800

    新增 MBController 並示範 TempData 的使用方式

---
+++ WebApplication1/Controllers/MBController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MBController : Controller
+    {
+        // GET: MB
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ViewResultTestModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["Message"] = "您輸入的姓名為 " + model.Name;
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
