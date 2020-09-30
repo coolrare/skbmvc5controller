202d2a630f5507eb63d18e74eb0b0890b920da96
Will <doggy.huang@gmail.com>
2020-09-30 10:16:15 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected override void HandleUnknownAction(string actionName)
+        {
+            if (ControllerContext.HttpContext.Request.HttpMethod.ToUpper() == "GET")
+            {
+                Redirect("/").ExecuteResult(ControllerContext);
+            }
+            else
+            {
+                base.HandleUnknownAction(actionName);
+            }
+        }
+    }
+}
