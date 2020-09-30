752bff63817e3a37d50732bf634ab1ab409a824c
Will <doggy.huang@gmail.com>
2020-09-30 11:44:12 +0800

    加入 ADO.NET 實體資料模型

---
+++ WebApplication1/Models/ViewResultTestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class ViewResultTestModel
+    {
+        public int Id { get; set; } = 1;
+
+        public string Name { get; set; } = "Will";
+    }
+}
